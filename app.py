05493b06c18e3cc4447be0288792436cb4518929
Th3Coder <th3noob8389@gmail.com>
2024-01-27 15:45:44 +0530

    Minor Improvements & Bug Fixes ^_^

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ currently_crawling_pages = set()
 def crawl_web(url, depth=2):
     visited_pages = set()
     base_url = urlparse(url).scheme + '://' + urlparse(url).hostname
+    website_name = urlparse(url).hostname.split('.')[0]  # Extract website name from the URL
     zip_buffer = BytesIO()
 
     with ZipFile(zip_buffer, 'a') as zip_file:
-
         def recursive_crawl(current_url, current_depth):
             if current_url in visited_pages or current_url in currently_crawling_pages or current_depth > depth:
                 return
@@ -25,21 +25,22 @@ def crawl_web(url, depth=2):
             currently_crawling_pages.add(current_url)
 
             try:
-                response = requests.get(current_url)
-                soup = BeautifulSoup(response.text, 'html.parser')
+                with requests.get(current_url, stream=True) as response:
+                    response.raise_for_status()
+                    soup = BeautifulSoup(response.text, 'html.parser')
 
-                for link in soup.find_all('a'):
-                    next_url = link.get('href')
-                    if next_url:
-                        absolute_url = urljoin(base_url, next_url)
-                        if absolute_url.startswith(base_url):
-                            recursive_crawl(absolute_url, current_depth + 1)
+                    for link in soup.find_all('a'):
+                        next_url = link.get('href')
+                        if next_url:
+                            absolute_url = urljoin(base_url, next_url)
+                            if absolute_url.startswith(base_url):
+                                recursive_crawl(absolute_url, current_depth + 1)
 
-                content = response.content
-                file_name = current_url.rsplit('/', 1)[-1].replace('/', '_') + ".html"
-                zip_file.writestr(file_name, content)
+                    content = response.content
+                    file_name = current_url.rsplit('/', 1)[-1].replace('/', '_') + ".html"
+                    zip_file.writestr(file_name, content)
 
-            except Exception as e:
+            except requests.exceptions.RequestException:
                 pass
 
             finally:
@@ -49,7 +50,7 @@ def crawl_web(url, depth=2):
 
         recursive_crawl(url, 1)
 
-    return zip_buffer.getvalue()
+    return zip_buffer.getvalue(), website_name
 
 @app.route('/')
 def index():
@@ -59,13 +60,9 @@ def index():
 def crawl():
     url_to_crawl = request.form['url']
     depth = int(request.form['depth'])
-    zip_data = crawl_web(url_to_crawl, depth)
+    zip_data, website_name = crawl_web(url_to_crawl, depth)
 
-    return send_file(BytesIO(zip_data), download_name='crawled_data.zip', as_attachment=True)
-
-@socketio.on('update_pages_request')
-def handle_update_pages_request():
-    emit('update_pages', list(currently_crawling_pages))
+    return send_file(BytesIO(zip_data), download_name=f'{website_name}_crawled_data.zip', as_attachment=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
